90ba2ccb487a23449b43a4daf7b0586164c46f7c
miledaltonmtl <michael.j.dalton@gmail.com>
2023-05-16 14:28:19 -0400

    install react-social-icons
    add header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <div>I am a Header</div>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20
+    xl:items-center">
+      <div className="flex flex-row items-center">
+        {/* Social Icons */}
+        <SocialIcon url="https://www.linkedin.com/in/mike-dalton-4a002224/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon url="https://github.com/mikedaltonmtl"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon url="https://github.com/mikedaltonmtl"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Contact Me
+        </p>
+      </div>
+    </header>
   );
 }
