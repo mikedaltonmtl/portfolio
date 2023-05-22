9a8906c0cb66ca0bd13ca85934362f1f1d249dcf
miledaltonmtl <michael.j.dalton@gmail.com>
2023-05-22 13:51:24 -0400

    adjuct image animation in about component

--- components/About.tsx
+++ components/About.tsx
@@ -13,7 +13,7 @@ export default function About() {
       </h3>
 
       <motion.img
-        initial={{ x: -200, opacity: 0 }}
+        initial={{ x: -300, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.8 }}
         src="/IMG_2205.jpg"
