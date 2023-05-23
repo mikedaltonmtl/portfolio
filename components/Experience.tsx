9f060e527d6a13a53f28e0ad7c5fdc236f9dca95
miledaltonmtl <michael.j.dalton@gmail.com>
2023-05-23 10:09:06 -0400

    add data to experience component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,10 +1,55 @@
 import { motion } from "framer-motion";
 import ExperienceCard from "./ExperienceCard";
 
+const experiences = [
+  {
+    url: "/NBC.jpg",
+    companyName: "National Bank Financial",
+    jobTitle: "Analyst, Reconciliation",
+    duration: "Sep 2010 - May 2013",
+    summary: [
+      "Production, development and automation of reconciliation and trading reports.",
+      "Analysis and resolution of discrepancies between accounting and auxiliary systems.",
+      "Creation of dashboard tools to improve the management of reports across the reconciliation group.",
+    ]
+  },
+  {
+    url: "/CDP.avif",
+    companyName: "Caisse de dépôt et placement du Québec",
+    jobTitle: "Analyst, Corporate Actions",
+    duration: "Nov 2007 - Sep 2010",
+    summary: [
+      "Processing Corporate Actions affecting holdings of the Caisse.",
+      "Daily liaison with Portfolio Managers to coordinate responses to voluntary actions and to advise of upcoming events",
+      "Verification and reconciliation of activity with custodian and counterparties.",
+    ]
+  },
+  {
+    url: "/RBC.png",
+    companyName: "RBC Direct Investing",
+    jobTitle: "Investment Services Representative",
+    duration: "Oct 2006 - Oct 2007",
+    summary: [
+      "Answering customer service and trading inquiries in fast-paced call center environment.",
+      "Providing information on market activity, products and customer accounts.",
+      "Working with customers to assist them in executing trades.",
+    ]
+  },
+  {
+    url: "/ISI.png",
+    companyName: "ISI",
+    jobTitle: "Junior Developer",
+    duration: "Jan 2005 - Jul 2006",
+    summary: [
+      "Production of Web-based applications involving the development of eCommerce Web sites.",
+      "Database optimisation and management.",
+      "Improvement and troubleshooting of existing projects.",
+      "Skills: Java, PHP, CSS, HTML, SQL"
+    ]
+  },
+];
 
-type Props = {}
-
-export default function Experience({}: Props) {
+export default function Experience() {
   
   return (
     <motion.div
@@ -13,16 +58,17 @@ export default function Experience({}: Props) {
       whileInView={{ opacity: 1 }}
       className="flex flex-col relative min-h-screen text-left overflow-hidden md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-20 uppercase tracking-[20px] text-gray-500 text-lg md:text-2xl">
+      <h3 className="pageName">
         &nbsp;Experience
       </h3>
 
-      <div className="w-full min-h-screen flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-thumb-rounded-[5px] scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/40">
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
+      <div className="w-[600px] h-screen pt-32 flex space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar scrollbar-thumb-rounded-[5px] scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/40">
+        {experiences.map(experiance => (
+          <ExperienceCard
+            key={experiance.url}
+            experience={experiance}
+          />
+        ))}
       </div>
     </motion.div>
   );
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,32 +1,31 @@
 import { motion } from "framer-motion";
 
-type Props = {}
 
-export default function ExperienceCard({}: Props) {
+export default function ExperienceCard({experience}: Props) {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity-200 overflow-hidden">
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[550px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity-200 overflow-hidden">
       <motion.img
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         className="w-32 h-32 rounded-full object-cover object-center"
-        src="/ISI.png"
+        src={experience.url}
       />
 
-      <div className="px-0 md:px-10">
-        <h4 className="text-3xl font-light">Company Name</h4>
-        <p className="font-bold text-2xl mt-0">Job Title</p>
-
-        <p className="uppercase py-2 text-gray-300">To/From Dates</p>
-
-        <ul className="list-disc space-y-2 ml-5 text-base">
-          <li>summary points</li>
-          <li>summary points</li>
-          <li>summary points</li>
-          <li>summary points</li>
+      <div className="px-0 md:px-10 w-[400px]">
+        <div className="w-[300px] text-center">
+          <h4 className="text-xl font-light">{experience.companyName}</h4>
+          <p className="font-semibold text-lg mt-0">{experience.jobTitle}</p>
+          <p className="uppercase pb-2 text-gray-300">{experience.duration}</p>
+        </div>
+        <ul className="list-disc space-y-2 ml-5 text-sm">
+          {experience.summary.map((bullet: String, index) => (
+            <li key={index}>
+              {bullet}
+            </li>
+          ))}
         </ul>
-
       </div>
     </article>
   );
