f4eb20b6a1be5b91a7c5685d076a8cd364fe27d7
miledaltonmtl <michael.j.dalton@gmail.com>
2023-05-26 09:31:26 -0400

    clean for deployment

--- components/About.tsx
+++ components/About.tsx
@@ -30,11 +30,11 @@ export default function About() {
         <h4 className="text-xl md:text-2xl font-semibold text-center md:text-justify">
           Here is a <span className="underline decoration-[#F7AB0A]/50">little</span> background
         </h4>
-        <p className="text-xs text-justify">
-          Hi there! I&apos;ve Mike, a recent graduated from a Full-stack Web Dev bootcamp. I&apos;ve had a great time learning a bunch of new skills which I can&apos;t wait to put to work!<br />
-          My background is in Finance, but prior to the bootcamp I was a stay-at-home-Dad, bringing up three young children. This was truly one of the best learning experiences of my life, during which time I developed some great, transferable skills such as time management, task prioritization and problem solving as well some that I hope aren&apos;t transferable, like blowing noses and changing diapers!<br />
-          The bootcamp gave me plenty of hands-on experience with HTML, CSS and JavaScript; while the introduction to React was love at first sight!
-        </p>
+        <section className="text-xs md:text-sm text-justify md:space-y-2">
+          <p>Hi there! I&apos;ve Mike, a recent graduated from a Full-stack Web Dev bootcamp. I&apos;ve had a great time learning a bunch of new skills which I can&apos;t wait to put to work!</p>
+          <p>My background is in Finance, but prior to the bootcamp I was a stay-at-home-Dad, bringing up three young children. This was truly one of the best learning experiences of my life, during which time I developed some great, transferable skills such as time management, task prioritization and problem solving as well some that I hope aren&apos;t transferable, like blowing noses and changing diapers!</p>
+          <p>The bootcamp gave me plenty of hands-on experience with HTML, CSS and JavaScript; while the introduction to React was love at first sight!</p>
+        </section>
       </div>
     </motion.div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,6 @@ import Experience from '../components/Experience';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
-import Link from 'next/link';
-import { ArrowUpCircleIcon } from '@heroicons/react/24/solid';
-import { motion } from "framer-motion";
 import ScrollToTop from '../components/ScrollToTop';
 
 
