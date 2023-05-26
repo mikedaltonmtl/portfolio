192c20783816e4b5f476b1464f377bc9c6fe9c86
miledaltonmtl <michael.j.dalton@gmail.com>
2023-05-26 10:59:52 -0400

    retry for deployment

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -14,7 +14,7 @@ type Props = {
   experience: Job;
 }
 
-export default function ExperienceCard(props: Props) {
+export default function ExperienceCard({ experience, key }: Props) {
   return (
     <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] snap-center bg-[#292929] md:p-5 hover:opacity-100 opacity-40 cursor-pointer transition-opacity-200 overflow-hidden">
       <motion.img
@@ -23,17 +23,17 @@ export default function ExperienceCard(props: Props) {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         className="w-32 h-32 rounded-full object-cover object-center pt-2"
-        src={props.experience.url}
+        src={experience.url}
       />
 
       <div className="mx-24 w-[300px] md:px-10 md:w-[400px]">
         <div className="w-[300px] text-center">
-          <h4 className="text-xl font-light">{props.experience.companyName}</h4>
-          <p className="font-semibold text-lg mt-0">{props.experience.jobTitle}</p>
-          <p className="uppercase pb-2 text-gray-300">{props.experience.duration}</p>
+          <h4 className="text-xl font-light">{experience.companyName}</h4>
+          <p className="font-semibold text-lg mt-0">{experience.jobTitle}</p>
+          <p className="uppercase pb-2 text-gray-300">{experience.duration}</p>
         </div>
         <ul className="list-disc space-y-2 ml-5 text-sm">
-          {props.experience.summary.map((bullet, index) => (
+          {experience.summary.map((bullet, index) => (
             <li key={index}>
               {bullet}
             </li>
